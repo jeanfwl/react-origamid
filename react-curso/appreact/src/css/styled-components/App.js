69b1e37568dfef04c516b6e944c1e9c246b99d16
Jean Talar <jeanttalar@gmail.com>
2023-04-11 21:07:20 -0300

    Finalizado aula de styled components

--- react-curso/appreact/src/css/styled-components/App.js
+++ react-curso/appreact/src/css/styled-components/App.js
@@ -10,27 +10,55 @@ const Produto = styled.div`
 `;
 
 const Titulo = styled.h1`
-  font-size: 2rem;
+  font-size: 3rem;
   color: tomato;
 `;
 
 const Preco = styled.p`
-  font-size: 1.5rem;
-  color: green;
+  font-size: ${({ promocao }) => (promocao ? '4rem' : '2.5rem')};
+  color: ${({ cor }) => cor};
+  font-weight: bold;
+`;
+
+const BotaoPromocao = styled.button`
+  background-color: lightseagreen;
+  padding: 1rem;
+  font-size: 2rem;
+  border-radius: 1rem;
+  border: none;
+  cursor: pointer;
+  outline: 0;
+  &:hover {
+    background-color: goldenrod;
+  }
+  &:focus {
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 1);
+  }
 `;
 
 const App = () => {
+  const [promocao, setPromocao] = React.useState(false);
+
   return (
-    <ProdutosContainer>
-      <Produto>
-        <Titulo>Papel toalha</Titulo>
-        <Preco>R$ 123,00</Preco>
-      </Produto>
-      <Produto>
-        <Titulo>Papel higiênico</Titulo>
-        <Preco>R$ 122,00</Preco>
-      </Produto>
-    </ProdutosContainer>
+    <>
+      <BotaoPromocao onClick={() => setPromocao(!promocao)}>
+        Incluir Promoção
+      </BotaoPromocao>
+      <ProdutosContainer>
+        <Produto>
+          <Titulo>Papel toalha</Titulo>
+          <Preco cor="green" promocao={promocao}>
+            R$ 123,00
+          </Preco>
+        </Produto>
+        <Produto>
+          <Titulo>Papel higiênico</Titulo>
+          <Preco cor="blue" promocao={promocao}>
+            R$ 122,00
+          </Preco>
+        </Produto>
+      </ProdutosContainer>
+    </>
   );
 };
 
