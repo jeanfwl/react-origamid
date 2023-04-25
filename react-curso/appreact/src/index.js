a441ba57f90c9eb981269435f1ffd7d9b2856c3a
Jean Talar <jeanttalar@gmail.com>
2023-04-24 21:45:49 -0300

    Realizada aula de Lazy e Suspense

--- react-curso/appreact/src/index.js
+++ react-curso/appreact/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './mais-react/propTypes/App';
+import App from './mais-react/lazy-and-suspense/App';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min'; possível omitir usando react-bootstrap
