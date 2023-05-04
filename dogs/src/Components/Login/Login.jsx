0fa83d5b9d02152f8fc2e56b00c1a3d06c6e6ad7
Jean Paulo Talar <jeanttalar@gmail.com>
2023-05-04 16:57:19 -0300

    Adicionado rotas e componentes iniciais para o Login

--- dogs/src/Components/Login/Login.jsx
+++ dogs/src/Components/Login/Login.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/criar" element={<LoginCreate />} />
+        <Route path="/perdeu" element={<LoginPasswordLost />} />
+        <Route path="/resetar" element={<LoginPasswordReset />} />
+      </Routes>
+      <Link to="criar">Cadastrar</Link>
+      <Link to="perdeu">Esqueci a senha carai kkk</Link>
+    </div>
+  );
 };
 
 export default Login;
---
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch('https://dogsapi.origamid.dev/json/jw-auth/v1/token');
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Logar</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
