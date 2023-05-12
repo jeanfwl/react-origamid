3bda7a45857f5834930edc3e5137262eaa8142df
Jean Talar <jeanttalar@gmail.com>
2023-05-11 21:27:42 -0300

    Estilizado background do Login

--- dogs/src/Components/Login/Login.jsx
+++ dogs/src/Components/Login/Login.jsx
@@ -5,21 +5,24 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { loggedIn } = React.useContext(UserContext);
 
   if (loggedIn) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/criar" element={<LoginCreate />} />
-        <Route path="/perdeu" element={<LoginPasswordLost />} />
-        <Route path="/resetar" element={<LoginPasswordReset />} />
-      </Routes>
-      <Link to="perdeu">Esqueci a senha carai kkk</Link>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/criar" element={<LoginCreate />} />
+          <Route path="/perdeu" element={<LoginPasswordLost />} />
+          <Route path="/resetar" element={<LoginPasswordReset />} />
+        </Routes>
+        <Link to="perdeu">Esqueci a senha carai kkk</Link>
+      </div>
+    </section>
   );
 };
 
