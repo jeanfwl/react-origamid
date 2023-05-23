407dba030e94d4e5093cbe36f9354b08164f8558
Jean <jeanttalar@gmail.com>
2023-05-23 20:15:58 -0300

    Adicionado componente para Protected Routes e adicionado rota + componente Account

--- dogs/src/App.jsx
+++ dogs/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import './App.css';
 import { UserStorage } from './UserContext';
+import Account from './Components/Account/Account';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 const App = () => {
   return (
@@ -14,7 +16,15 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
+          <Route path="login/*" element={<Login />} />
+          <Route
+            path="conta/*"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
         <Footer />
       </UserStorage>
---
+++ dogs/src/Components/Helper/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { UserContext } from '../../UserContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { loggedIn } = React.useContext(UserContext);
+  if (loggedIn === true) {
+    return children;
+  } else if (loggedIn === false) {
+    return <Navigate to="/login" />;
+  } else {
+    return <></>;
+  }
+};
+
+export default ProtectedRoute;
